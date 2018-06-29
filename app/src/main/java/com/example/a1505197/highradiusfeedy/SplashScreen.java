2ab5ef6886e16efc949f632d5131cafb86667504
Vivek <vivekkumarsharma9746@gmail.com>
2018-06-29 17:30:37 +0530

    bcd

--- app/src/main/java/com/example/a1505197/highradiusfeedy/SplashScreen.java
+++ app/src/main/java/com/example/a1505197/highradiusfeedy/SplashScreen.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
