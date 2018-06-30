f2b626d2a15f74fbb392eb7fb287512947ef41d9
Vivek <vivekkumarsharma9746@gmail.com>
2018-06-30 14:11:47 +0530

    network connection check in splash screen

--- app/src/main/java/com/example/a1505197/highradiusfeedy/SplashScreen.java
+++ app/src/main/java/com/example/a1505197/highradiusfeedy/SplashScreen.java
@@ -1,5 +1,11 @@
 package com.example.a1505197.highradiusfeedy;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,23 +16,90 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new android.os.Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+        if(isWorkingInternetPersent()){
+            splash();
+        }
+        else{
+            showAlertDialog(SplashScreen.this, "Network Error",
+                    "Please, make sure you are having a working internet connection", false);
+        }
 
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                android.content.Intent i = new android.content.Intent(SplashScreen.this, Login.class);
-                startActivity(i);
 
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+
+
+//        new Handler().postDelayed(new Runnable() {
+//
+//            public void run() {
+//                // This method will be executed once the timer is over
+//                // Start your app main activity
+//                Intent i = new Intent(SplashScreenActivity.this, Login.class);
+//                startActivity(i);
+//
+//                // close this activity
+//                finish();
+//            }
+//
+//        },SPLASH_TIME_OUT);
     }
+
+
+    public void splash() {
+        Thread timerTread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerTread.start();
+    }
+
+    public boolean isWorkingInternetPersent() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getBaseContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+        // Setting alert dialog icon
+        // alertDialog.setIcon((status) ? R.mipmap.ic_launcher : R.mipmap.ic_launcher);
+
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
+                System.exit(0);
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+
 }
